ba49fc7460126ec34270c57f44e8ccb644bf4128
Administrator <Administrator@XTREMELITE-PC>
2025-03-03 19:44:00 +0500

    Добавьте файлы проекта.

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private static List<Car> Cars = new List<Car>
+        {
+            new Car { Id = 1, Make = "Toyota", Model = "Corolla", Year = 2020, VIN = "1HGCM82633A123456", IsAvailable = true },
+            new Car { Id = 2, Make = "Honda", Model = "Civic", Year = 2019, VIN = "2HGCM82633A654321", IsAvailable = false }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> GetCars()
+        {
+            return Ok(Cars);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Car> GetCar(int id)
+        {
+            var car = Cars.FirstOrDefault(c => c.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public ActionResult<Car> AddCar(Car car)
+        {
+            car.Id = Cars.Max(c => c.Id) + 1;
+            Cars.Add(car);
+            return CreatedAtAction(nameof(GetCar), new { id = car.Id }, car);
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCar(int id, Car updatedCar)
+        {
+            var car = Cars.FirstOrDefault(c => c.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            car.Make = updatedCar.Make;
+            car.Model = updatedCar.Model;
+            car.Year = updatedCar.Year;
+            car.VIN = updatedCar.VIN;
+            car.IsAvailable = updatedCar.IsAvailable;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCar(int id)
+        {
+            var car = Cars.FirstOrDefault(c => c.Id == id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            Cars.Remove(car);
+            return NoContent();
+        }
+    }
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string VIN { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Authentication.Negotiate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme)
+   .AddNegotiate();
+
+builder.Services.AddAuthorization(options =>
+{
+    // By default, all incoming requests will be authorized according to the default policy.
+    options.FallbackPolicy = options.DefaultPolicy;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
